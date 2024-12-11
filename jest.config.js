749044af63b49e45aee4546da74c2aedd69c90a6
Aku Suvanto <41476300+akusuvanto@users.noreply.github.com>
2024-12-11 03:14:07 +0200

    Configure coverage to be reported based on test plan

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const config = {
+    collectCoverageFrom: [
+      'src/add.js',
+      'src/defaultTo.js',
+      'src/map.js',
+      'src/reduce.js',
+      'src/upperFirst.js',
+      'src/endsWith.js',
+      'src/toInteger.js',
+      'src/filter.js'
+    ],
+  };
+  
+  module.exports = config;
